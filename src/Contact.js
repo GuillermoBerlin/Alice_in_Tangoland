67b99a3108adc45ed9575fb12fa60af213e96e91
guillermo.333 <guillermo.3@hotmail.com.ar>
2023-07-08 01:24:11 +0200

    feature/#5: set layout placeholders.

--- src/Contact.js
+++ src/Contact.js
@@ -5,9 +5,9 @@ import './contact.css';
 export default function Contact() {
   return (
     <div className='contactMainDiv'>
-      <h1>CONTACT</h1>
-      <h1>CONTACT</h1>
-      <h1>CONTACT</h1>
+      <h3>name</h3>
+      <h3>e-mail</h3>
+      <h3>text field</h3>
     </div>
   )
 }
