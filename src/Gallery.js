97c84089efe16a2a2173f380fbbee3ff596cdbdf
albanodirosa <albanodirosa90@gmail.com>
2023-07-07 19:24:31 -0300

    Add lightbox to gallery section.

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,12 +1,59 @@
-import React from 'react'
-import './gallery.css';
+import React, { useState } from "react";
+import "./gallery.css";
+import images from "./Images";
 
 export default function Gallery() {
+  const [imageToShow, setImageToShow] = useState("");
+  const [lightboxDisplay, setLightBoxDisplay] = useState(false);
+
+  const showImage = (image) => {
+    setImageToShow(image);
+    setLightBoxDisplay(true);
+  };
+
+  const hideLightBox = () => {
+    setLightBoxDisplay(false);
+  };
+
+  const imageCards = images.map((image) => (
+    <img className="image-card" onClick={() => showImage(image)} src={image} />
+  ));
+
+  const showNext = (e) => {
+    e.stopPropagation();
+    let currentIndex = images.indexOf(imageToShow);
+    if (currentIndex >= images.length - 1) {
+      setLightBoxDisplay(false);
+    } else {
+      let nextImage = images[currentIndex + 1];
+      setImageToShow(nextImage);
+    }
+  };
+
+  const showPrev = (e) => {
+    e.stopPropagation();
+    let currentIndex = images.indexOf(imageToShow);
+    if (currentIndex <= 0) {
+      setLightBoxDisplay(false);
+    } else {
+      let nextImage = images[currentIndex - 1];
+      setImageToShow(nextImage);
+    }
+  };
+
   return (
-    <div className='galleryMainDiv'>
-      <h1>GALLERY</h1>
-      <h1>GALLERY</h1>
-      <h1>GALLERY</h1>
-    </div>
-  )
+    <>
+      <div className="wrapper">{imageCards}</div>
+
+      {lightboxDisplay ? (
+        <div id="lightbox" onClick={hideLightBox}>
+          <button onClick={showPrev}>⭠</button>
+          <img id="lightbox-img" src={imageToShow}></img>
+          <button onClick={showNext}>⭢</button>
+        </div>
+      ) : (
+        ""
+      )}
+    </>
+  );
 }
