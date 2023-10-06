e100d6f7b9635ce0e23a181b3cb276a1bbe10412
albanodirosa <albanodirosa90@gmail.com>
2023-10-06 20:53:58 -0300

    Resolves #18

--- src/components/Scafold.js
+++ src/components/Scafold.js
@@ -5,41 +5,49 @@ import { FiMenu } from "react-icons/fi";
 import { IoCloseSharp } from "react-icons/io5";
 import florishMenu from "../images/florish.png";
 
-
 function Scafold() {
-  const [isActive, setIsActive] = useState({about: true});
+  const [isActive, setIsActive] = useState({ about: true });
 
   const handleToggle = (e) => {
     if (e.target.value !== Object.keys(isActive)[0]) {
       setIsActive(() => ({ [e.target.value]: !isActive[e.target.value] }));
-      console.log(isActive)
+      console.log(isActive);
     }
   };
 
   const [click, setClick] = useState(false);
   const handleClick = () => setClick(!click);
 
-
   return (
     <div className="main-container-menu">
-      <div onClick={handleClick} className={click ? "buttons-container active" : "buttons-container" }>
+      <div
+        onClick={handleClick}
+        className={click ? "buttons-container active" : "buttons-container"}
+      >
         <Link to="/about">
           <button
             value="about"
             onClick={handleToggle}
-            
             className={isActive["about"] ? "activeButton" : "Button"}
-          >SHOW</button>
+          >
+            SHOW
+          </button>
         </Link>
-        
+        <Link to="/staff">
+          <button
+            value="staff"
+            onClick={handleToggle}
+            className={isActive["staff"] ? "activeButton" : "Button"}
+          >
+            ELENCO
+          </button>
+        </Link>
 
         <Link to="/gallery">
           <button
             value="gallery"
             onClick={handleToggle}
-            className={
-              isActive["gallery"] ? "activeButton" : "Button"
-            }
+            className={isActive["gallery"] ? "activeButton" : "Button"}
           >
             GALERÍA
           </button>
@@ -48,27 +56,23 @@ function Scafold() {
           <button
             value="contact"
             onClick={handleToggle}
-            className={
-              isActive["contact"] ? "activeButton" : "Button"
-            }
+            className={isActive["contact"] ? "activeButton" : "Button"}
           >
             CONTACTO
           </button>
         </Link>
 
-        <img src={florishMenu} alt="florish" className="florish-menu"/>
-        
+        <img src={florishMenu} alt="florish" className="florish-menu" />
+
         <div className="icon-cancel-container" onClick={handleClick}>
-          <IoCloseSharp   size={50} className="icon-cancel"/>
-      </div>
+          <IoCloseSharp size={50} className="icon-cancel" />
+        </div>
       </div>
       <div onClick={handleClick}>
-            <FiMenu   size={50} className="icon-menu"/>
+        <FiMenu size={50} className="icon-menu" />
       </div>
     </div>
   );
 }
 
 export default Scafold;
-
-
---
+++ src/pages/subpages/OnStage.js
@@ -0,0 +1,76 @@
+import "./subpages.css";
+
+export default function OnStage() {
+  return (
+    <div className="main-container">
+      <p className="bio-container">
+        Natasha Barbara (Argentina - 1991):<br></br>Artista del movimiento,
+        bailarina de Tango Argentino con más de 20 años de experiencia, su
+        expresividad e interpretación dan a cada uno de sus movimientos fuerza y
+        autenticidad. Natasha es Alice.
+      </p>
+      <br></br>
+      <p className="bio-container">
+        Luciano De Esbornia (Argentina - 1986):<br></br>Artista
+        multidisciplinario, malabarista, manipulaciones de objetos, y bailarín
+        de tango argentino, su estilo moderno, lúdico y elegante destaca en su
+        forma única de bailar. Luciano es el Maestro & “el Zapatero loco”.
+      </p>
+      <br></br>
+      <p className="bio-container">
+        Emmanuel Casal & Yanina Muzyka (Argentina - 1987/1985):<br></br>Una
+        pareja excepcionalmente virtuosa y original. Bailando juntos por los
+        últimos 14 años, comparten una complicidad y armonía extraordinaria. Su
+        estilo único los consagró campeones mundiales de Tango Escenario,
+        edición 2021. Emmanuel es El Conejo Blanco. Yanina es La Reina de
+        Corazones.
+      </p>
+      <br></br>
+      <p className="bio-container">
+        Pablo Solari (Argentina - 1984):<br></br>Artista de Circo, especialista
+        en Cyrwheel (rueda Cyr), una de las disciplinas más difíciles del circo,
+        desafiando las leyes de la gravedad. A su vez, es bailarín de “contact”
+        y tango argentino, su recorrido artístico hace su tango muy acrobático.
+        Pablo es el Gato, the Cheshire Cat.
+      </p>
+      <br></br>
+      <p className="bio-container">
+        Martha Battaller (España - 1982):<br></br>Artista de circo, aerialista y
+        contorsionista en pole. Su gracia y su personalidad incomparable, la
+        llevaron a trabajar por diversas compañías y sitios de los más
+        prestigiosos, situandola como una de las mejores en su disciplina. Marta
+        es la Oruga & la Mariposa.
+      </p>
+      <br></br>
+      <p className="bio-container">
+        the PodziSisters - Tania & Naomi Podzi (España - 1996/1994):<br></br>
+        Aerialistas, acróbatas y contorsionistas. Nacidas y criadas en Ibiza,
+        muy conocidas en la isla e internacionalmente.¡Las hermanas Podzi son
+        artistas traviesas y fascinantes cuyas habilidades te dejarán
+        boquiabierto! Tania & Naomi son las gemelas.
+      </p>
+      <br></br>
+      <p className="bio-container">
+        Fibi Eyewalker (Israel - 1988):<br></br>Artista de circo, Tragasable. En
+        los últimos años, Tragasables, uno de los arte circenses más antiguos,
+        se ha ido integrando cada vez más en espectáculos, para un público más
+        amplio, encontrando poco a poco la distinción y el respeto que merece.
+        ¡Fibi es única! Hipnotizante y con un talento fuera de lo común. Desde
+        los “Freak shows” de Coney Island, Nueva York, hasta el mundo, su
+        presencia en cualquier obra, garantiza un momento sensacional lleno de
+        emociones y escalofríos. Fibi es La Verduga (the knave of heart).
+      </p>
+      <br></br>
+      <p className="bio-container">
+        The Flakinys - Gonza & Marina (Argentina - 1983/1982):<br></br>Artistas
+        de circo, clowns, zanqueros, malabares, monociclo. Los Flakinys
+        dedicaron los ultimos 20 años a las artes escénicas, transitando
+        distintos roles y disciplinas. Desde hace 10 años, trabajan juntos,
+        haciendo espectáculos y eventos de danza y circo a nivel internacional.
+        También dirigen un espacio cultural, Tierra Marina, donde se dictan
+        clases, seminarios y eventos culturales. The Flakinys son las flores.
+        Gonza es el Huron. Marina es una de los naipes.
+      </p>
+    </div>
+  );
+}
